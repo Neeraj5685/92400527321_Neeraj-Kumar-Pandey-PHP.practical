1d097883fac9c453e04d7c5c73c1f53b0e264b6d
Neeraj5685 <neerajpandey56085@gmail.com>
2026-06-23 22:48:26 +0530

    Add files via upload

---
+++ lab_work_1.2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = 25;
+$num2 = 10;
+$num3 = 40;
+
+$max = max($num1, $num2, $num3);
+$min = min($num1, $num2, $num3);
+
+echo "Maximum Number = " . $max . "<br>";
+echo "Minimum Number = " . $min;
+?>
