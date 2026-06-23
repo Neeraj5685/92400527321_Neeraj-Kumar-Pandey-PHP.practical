8ab8f6ab38599981072677acf6a2fb088b7821ca
Neeraj5685 <neerajpandey56085@gmail.com>
2026-06-23 22:45:53 +0530

    Add files via upload

---
+++ lab_work_1.1.php
@@ -0,0 +1,13 @@
+<?php
+define("SEMESTER", "Semester 4");
+
+$name = "Neeraj Kumar Pandey";
+$percentage = 78.5;
+$result = "PASS";
+
+echo "<h3>Previous Semester Result</h3>";
+echo "Name: " . $name . "<br>";
+echo "Semester: " . SEMESTER . "<br>";
+echo "Percentage: " . $percentage . "%<br>";
+echo "Result: " . $result;
+?>
