abbed300021e289df1a0e41852b41aa038f8dbc0
Neeraj5685 <neerajpandey56085@gmail.com>
2026-06-25 00:37:41 +0530

    Update lab_work_1.1.php

--- lab_work_1.1.php
+++ lab_work_1.1.php
@@ -5,9 +5,9 @@ $name = "Neeraj Kumar Pandey";
 $percentage = 78.5;
 $result = "PASS";
 
-echo "<h3>Previous Semester Result</h3>";
-echo "Name: " . $name . "<br>";
-echo "Semester: " . SEMESTER . "<br>";
-echo "Percentage: " . $percentage . "%<br>";
-echo "Result: " . $result;
+echo "Previous Semester Result\n";
+echo "Name: $name\n";
+echo "Semester: " . SEMESTER . "\n";
+echo "Percentage: $percentage%\n";
+echo "Result: $result\n";
 ?>
